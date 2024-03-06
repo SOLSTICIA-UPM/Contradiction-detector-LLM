be13105e7ad705f25a019a0d18350015707ca573
carlotardelasen <carlota.rdelasen@alumnos.upm.es>
2024-03-06 11:46:31 +0100

    Actualización incluyendo carpeta llama2

---
+++ Contradiction-detector-llama2/060324_llama2.py
@@ -0,0 +1,22 @@
+import requests
+
+api_url = "https://api-inference.huggingface.co/models/meta-llama/Llama-2-70b-chat-hf"
+question = "What is the meaning of life?"
+token = "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"
+
+# Definir el encabezado de autorización
+headers = {"Authorization": f"Bearer {token}"}
+
+# Cuerpo de la solicitud
+data = {"inputs": question}
+
+# Realizar la solicitud POST a la API
+response = requests.post(api_url, headers=headers, json=data)
+
+# Obtener la respuesta del modelo
+if response.status_code == 200:
+    resultados = response.json()
+    respuesta = resultados[0]["generated_text"]
+    print("Respuesta del modelo:", respuesta)
+else:
+    print("Error al hacer la solicitud:", response.status_code)
